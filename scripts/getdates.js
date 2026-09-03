13d8d63c3c74d6fc67b44018afe3d85239cc5531
eze01061985 <jfloresmenendez@byupathway.edu>
2026-09-03 13:22:06 -0300

    js

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+const currentYear = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+currentYear.textContent = new Date().getFullYear();
+lastModified.textContent = `Last Modification: ${document.lastModified}`;
